af1460b03a7facce7cf4ce1def8178ab76457572
Nawal Reza <nawalreza07@gmail.com>
2024-05-14 23:24:11 -0400

    Add Initial Files

---
+++ Colledge App/code.js
@@ -0,0 +1,562 @@
+//Events for opening Starting Screens, Forgettting Passwords
+onEvent("SignUpStartingScreen", "click", function(event) {
+  setScreen("SignUpScreen");
+});
+onEvent("SignUpButton", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("SignInButton", "click", function(event) {
+  setScreen("SignInScreen");
+});
+onEvent("ForgotPassword_Button", "click", function(event) {
+  
+});
+onEvent("PasswordSubmission", "click", function(event) {
+  setScreen("SignInScreen");
+});
+onEvent("SignIn_Button", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("LogOut", "click", function(event) {
+  setScreen("SignInScreen");
+});
+
+//Opening and Going Back on College Tab
+onEvent("CollegeTab", "click", function(event) {
+  setScreen("CollegeTabPage#1");
+});
+onEvent("NextPage(College)_Button", "click", function(event) {
+  setScreen("CollegeTabPage#2");
+});
+onEvent("PreviousPage(College)_Button", "click", function(event) {
+  setScreen("CollegeTabPage#1");
+});
+onEvent("College#1(Info)_Button", "click", function(event) {
+  setScreen("College#1Info");
+});
+onEvent("GoBack#1(College)_Button", "click", function(event) {
+  setScreen("CollegeTabPage#1");
+});
+onEvent("Location#1_Button", "click", function(event) {
+  setScreen("College#1Location");
+});
+onEvent("College#2(Info)_Button", "click", function(event) {
+  setScreen("College#2Info");
+});
+onEvent("GoBack#2(College)_Button", "click", function(event) {
+  setScreen("CollegeTabPage#1");
+});
+onEvent("Location#2_Button", "click", function(event) {
+  setScreen("College#2Location");
+});
+onEvent("College#3(Info)_Button", "click", function(event) {
+  setScreen("College#3Info");
+});
+onEvent("GoBack#3(College)_Button", "click", function(event) {
+  setScreen("CollegeTabPage#1");
+});
+onEvent("Location#3_Button", "click", function(event) {
+  setScreen("College#3Location");
+});
+onEvent("College#4(Info)_Button", "click", function(event) {
+  setScreen("College#4Info");
+});
+onEvent("GoBack#4(College)_Button", "click", function(event) {
+  setScreen("CollegeTabPage#1");
+});
+onEvent("Location#4_Button", "click", function(event) {
+  setScreen("College#4Location");
+});
+onEvent("College#5(Info)_Button", "click", function(event) {
+  setScreen("College#5Info");
+});
+onEvent("GoBack#5(College)_Button", "click", function(event) {
+  setScreen("CollegeTabPage#1");
+});
+onEvent("Location#5_Button", "click", function(event) {
+  setScreen("College#5Location");
+});
+onEvent("College#6(Info)_Button", "click", function(event) {
+  setScreen("College#6Info");
+});
+onEvent("GoBack#6(College)_Button", "click", function(event) {
+  setScreen("CollegeTabPage#2");
+});
+onEvent("Location#6_Button", "click", function(event) {
+  setScreen("College#6Location");
+});
+onEvent("College#7(Info)_Button", "click", function(event) {
+  setScreen("College#7Info");
+});
+onEvent("GoBack#7(College)_Button", "click", function(event) {
+  setScreen("CollegeTabPage#2");
+});
+onEvent("Location#7_Button", "click", function(event) {
+  setScreen("College#7Location");
+});
+onEvent("College#8(Info)_Button", "click", function(event) {
+  setScreen("College#8Info");
+});
+onEvent("GoBack#8(College)_Button", "click", function(event) {
+  setScreen("CollegeTabPage#2");
+});
+onEvent("Location#8_Button", "click", function(event) {
+  setScreen("College#8Location");
+});
+onEvent("College#9(Info)_Button", "click", function(event) {
+  setScreen("College#9Info");
+});
+onEvent("GoBack#9(College)_Button", "click", function(event) {
+  setScreen("CollegeTabPage#2");
+});
+onEvent("Location#9_Button", "click", function(event) {
+  setScreen("College#9Location");
+});
+onEvent("College#10(Info)_Button", "click", function(event) {
+  setScreen("College#10Info");
+});
+onEvent("GoBack#10(College)_Button", "click", function(event) {
+  setScreen("CollegeTabPage#2");
+});
+onEvent("Location#10_Button", "click", function(event) {
+  setScreen("College#10Location");
+});
+
+//Opening and Going Back on Location Tab
+onEvent("LocationTab", "click", function(event) {
+  setScreen("LocationTabPage#1");
+});
+onEvent("NextPage(Location)_Button", "click", function(event) {
+  setScreen("LocationTabPage#2");
+});
+onEvent("PreviousPage(Location)_Button", "click", function(event) {
+  setScreen("LocationTabPage#1");
+});
+onEvent("Location_College#1", "click", function(event) {
+  setScreen("College#1Location");
+});
+onEvent("GoBack#1(Location)__Button", "click", function(event) {
+  setScreen("LocationTabPage#1");
+});
+onEvent("Info_College#1", "click", function(event) {
+  setScreen("College#1Info");
+});
+onEvent("Location_College#2", "click", function(event) {
+  setScreen("College#2Location");
+});
+onEvent("GoBack#2(Location)_Button", "click", function(event) {
+  setScreen("LocationTabPage#1");
+});
+onEvent("Info_College#2", "click", function(event) {
+  setScreen("College#2Info");
+});
+onEvent("Location_College#3", "click", function(event) {
+  setScreen("College#3Location");
+});
+onEvent("GoBack#3(Location)_Button", "click", function(event) {
+  setScreen("LocationTabPage#1");
+});
+onEvent("Info_College#3", "click", function(event) {
+  setScreen("College#3Info");
+});
+onEvent("Location_College#4", "click", function(event) {
+  setScreen("College#4Location");
+});
+onEvent("GoBack#4(Location)_Button", "click", function(event) {
+  setScreen("LocationTabPage#1");
+});
+onEvent("Info_College#4", "click", function(event) {
+  setScreen("College#4Info");
+});
+onEvent("Location_College#5", "click", function(event) {
+  setScreen("College#5Location");
+});
+onEvent("GoBack#5(Location)_Button", "click", function(event) {
+  setScreen("LocationTabPage#1");
+});
+onEvent("Info_College#5", "click", function(event) {
+  setScreen("College#5Info");
+});
+onEvent("Location_College#6", "click", function(event) {
+  setScreen("College#6Location");
+});
+onEvent("GoBack#6(Location)_Button", "click", function(event) {
+  setScreen("LocationTabPage#2");
+});
+onEvent("Info_College#6", "click", function(event) {
+  setScreen("College#6Info");
+});
+onEvent("Location_College#7", "click", function(event) {
+  setScreen("College#7Location");
+});
+onEvent("GoBack#7(Location)_Button", "click", function(event) {
+  setScreen("LocationTabPage#2");
+});
+onEvent("Info_College#7", "click", function(event) {
+  setScreen("College#7Info");
+});
+onEvent("Location_College#8", "click", function(event) {
+  setScreen("College#8Location");
+});
+onEvent("GoBack#8(Location)_Button", "click", function(event) {
+  setScreen("LocationTabPage#2");
+});
+onEvent("Info_College#8", "click", function(event) {
+  setScreen("College#8Info");
+});
+onEvent("Location_College#9", "click", function(event) {
+  setScreen("College#9Location");
+});
+onEvent("GoBack#9(Location)__Button", "click", function(event) {
+  setScreen("LocationTabPage#2");
+});
+onEvent("Info_College#9", "click", function(event) {
+  setScreen("College#9Info");
+});
+onEvent("Location_College#10", "click", function(event) {
+  setScreen("College#10Location");
+});
+onEvent("GoBack#10(Location)_Button", "click", function(event) {
+  setScreen("LocationTabPage#2");
+});
+onEvent("Info_College#10", "click", function(event) {
+  setScreen("College#10Info");
+});
+
+//Clicking on Home Icon
+onEvent("Home_Button", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#1", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#2", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#3", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#4", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#5", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#6", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#7", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#8", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#9", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#10", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#11", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#12", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#13", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#14", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#15", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#16", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#18", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#19", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#20", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#21", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#22", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#23", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#24", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#25", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#26", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#27", "click", function(event) {
+  setScreen("HomeScreem");
+});
+onEvent("Home_Button#28", "click", function(event) {
+  setScreen("HomeSreen");
+});
+onEvent("Home_Button#29", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#30", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#31", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#32", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#33", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#34", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#35", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#36", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#37", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#38", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#39", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#40", "click", function(event) {
+  setScreen("HomeScreen");
+});
+onEvent("Home_Button#41", "click", function(event) {
+  setScreen("HomeScreen");
+});
+
+//Clicking User Icon
+onEvent("Profile_Button", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#1", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#2", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#3", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#4", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#5", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#6", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#7", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#8", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#9", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#10", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#11", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#12", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#13", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#14", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#15", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#16", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#17", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#18", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#19", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#20", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#21", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#22", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#23", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#24", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#25", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#26", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#27", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#28", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#29", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#30", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#31", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#32", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#33", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profie_Button#34", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#35", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#36", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#37", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#38", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#39", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#40", "click", function(event) {
+  setScreen("ProfilePage");
+});
+onEvent("Profile_Button#41", "click", function(event) {
+  setScreen("ProfilePage");
+});
+
+//Opening and Going Back on Majors Tab
+onEvent("MajorsTab", "click", function(event) {
+  setScreen("MajorsTabPage#1");
+});
+onEvent("NextPage(Majors)_Button", "click", function(event) {
+  setScreen("MajorsTabPage#2");
+});
+onEvent("PreviousPage(Majors)_Button", "click", function(event) {
+  setScreen("MajorsTabPage#1");
+});
+onEvent("Major#1Info_Button", "click", function(event) {
+  setScreen("Major#1Info");
+});
+onEvent("GoBack#1(Majors)_Button", "click", function(event) {
+  setScreen("MajorsTabPage#1");
+});
+onEvent("Major#2Info_Button", "click", function(event) {
+  setScreen("Major#2Info");
+});
+onEvent("GoBack#2(Majors)_Button", "click", function(event) {
+  setScreen("MajorsTabPage#1");
+});
+onEvent("Major#3Info_Button", "click", function(event) {
+  setScreen("Major#3Info");
+});
+onEvent("GoBack#3(Majors)_Button", "click", function(event) {
+  setScreen("MajorsTabPage#1");
+});
+onEvent("Major#4Info_Button", "click", function(event) {
+  setScreen("Major#4Info");
+});
+onEvent("GoBack#4(Majors)_Button", "click", function(event) {
+  setScreen("MajorsTabPage#1");
+});
+onEvent("Major#5Info_Button", "click", function(event) {
+  setScreen("Major#5Info");
+});
+onEvent("GoBack#5(Majors)_Button", "click", function(event) {
+  setScreen("MajorsTabPage#1");
+});
+onEvent("Major#6Info_Button", "click", function(event) {
+  setScreen("Major#6Info");
+});
+onEvent("GoBack#6(Majors)_Button", "click", function(event) {
+  setScreen("MajorsTabPage#2");
+});
+onEvent("Major#7Info_Button", "click", function(event) {
+  setScreen("Major#7Info");
+});
+onEvent("GoBack#7(Majors)_Button", "click", function(event) {
+  setScreen("MajorsTabPage#2");
+});
+onEvent("Major#8Info_Button", "click", function(event) {
+  setScreen("Major#8Info");
+});
+onEvent("GoBack#8(Majors)_Button", "click", function(event) {
+  setScreen("MajorsTabPage#2");
+});
+onEvent("Major#9Info_Button", "click", function(event) {
+  setScreen("Major#9Info");
+});
+onEvent("GoBack#9(Majors)_Button", "click", function(event) {
+  setScreen("MajorsTabPage#2");
+});
+onEvent("Major#10Info_Button", "click", function(event) {
+  setScreen("Major#10Info");
+});
+onEvent("GoBack#10(Majors)_Button", "click", function(event) {
+  setScreen("MajorTabPage#2");
+});
+
+//Opening and Completeting Personality Test Tab
+onEvent("PersonalityTestTab", "click", function(event) {
+  setScreen("PersonalityTestTabInfo");
+});
+onEvent("NextPage(PersonalityTest)_Button", "click", function(event) {
+  setScreen("PersonalityTestPage1");
+});
+onEvent("NextPage#2(PersonalityTest)_Button", "click", function(event) {
+  setScreen("PersonalityTestPage2");
+});
+onEvent("Results_Button", "click", function(event) {
+  setScreen("PersonalityTestResults");
+});
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # FET_CollegeApp
 Developed an app to help students with looking up college stats and information in JavaScript and HTML
+
+The files in the Colledge App folder contains everything you need to run the app:
+
+* `applab/applab.css` - This file contains some default CSS styling.
+
+* `index.html` - This is the html file that contains all the buttons, text inputs, and other controls for your app. Go here to add additional controls to your app. Run this file to be able to load the application on a computer browser
+
+* `style.css` - This file defines the custom styling for each control in your
+  app that you specified to change the colors, positions, dimensions, and other properties of your app's controls.
+
+* `code.js` - This file has all the code you wrote for your app. Go here to add
+  new behavior to your app.
+
+* `assets` - This is a directory with all the asset files (pictures and sounds)
+  that was uploaded.
